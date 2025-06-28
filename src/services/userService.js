4f6f400a7f34e3d7b34dac3a05fd6e3765c9a9bc
pranjal-katkar <pranjalkatkar70@gmail.com>
2025-06-28 17:30:40 +0530

    Refactor: use .env for API base URL and rename userService.jsx to userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/users";
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}/api/users`;
 
 export const getUsers = () => axios.get(API_URL);
 
