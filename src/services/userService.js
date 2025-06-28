fe598097ff5fd7e0d85796b8b623e0158c5fc285
pranjal-katkar <pranjalkatkar70@gmail.com>
2025-06-28 21:49:55 +0530

    url updated

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api/users";
+const BASE_URL = "https://user-management-backend-bqat.onrender.com";
 
 export const getUsers = () => axios.get(BASE_URL);
 export const createUser = (user) => axios.post(BASE_URL, user);
